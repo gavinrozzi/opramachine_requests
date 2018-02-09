44cfcc27c51c5c3cdebb82dbd003a585c0b200b0
Gavin Rozzi <gr@gavinrozzi.com>
2018-02-09 15:49:04 -0500

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -2,8 +2,8 @@ require "scraperwiki"
 require "json"
 require "open-uri"
 
-FETCH_FROM_REQUEST_ID = 1
-FETCH_TO_REQUEST_ID = 1215
+FETCH_FROM_REQUEST_ID = 5
+FETCH_TO_REQUEST_ID = 1220
 base_url = "https://opramachine.com"
 
 (FETCH_FROM_REQUEST_ID..FETCH_TO_REQUEST_ID).each do |id|
